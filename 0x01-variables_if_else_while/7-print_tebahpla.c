8008468be5f8a02f8e90d1971013b733addad357
IfSheKnows <ifejacobray@gmail.com>
2023-07-14 21:19:36 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Begin code
+ * Return: 0 (Works fine)
+ */
+
+int main(void)
+{
+	char s;
+
+	s = 'z';
+	while
+		(s >= 'a') {
+			putchar(s);
+			s--;
+		}
+	putchar('\n');
+	return (0);
+}
