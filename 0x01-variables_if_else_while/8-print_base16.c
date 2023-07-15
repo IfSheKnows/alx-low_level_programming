138291301c7da02fe1c593cb104a4ca67bc99a75
IfSheKnows <ifejacobray@gmail.com>
2023-07-14 21:30:29 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Begin code
+ * Return: 0 (Works fine)
+ */
+
+int main(void)
+{
+	char a;
+
+	int b;
+
+	a = 'a';
+	b = '0';
+	while
+		(b < 10) {
+			putchar(b + '0');
+			b++;
+
+		}
+	while
+		(a <= 'f') {
+			putchar(a);
+			a++;
+		}
+	putchar('\n');
+	return (0);
+}
