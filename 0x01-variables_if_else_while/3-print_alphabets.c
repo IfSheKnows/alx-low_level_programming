f88dcb2d452916b86c2bbea95d051bd36034f023
IfSheKnows <ifejacobray@gmail.com>
2023-07-14 20:19:25 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Begin code
+ * Return: 0 (Successful)
+ */
+
+int main(void)
+{
+	char n;
+
+	char m;
+
+	n = 'a';
+	m = 'A';
+	while
+		(n <= 'z') {
+			putchar(n);
+			n++;
+		}
+	while
+		(m <= 'Z') {
+			putchar(m);
+			m++;
+		}
+	putchar('\n');
+	return (0);
+}
