206733c83737f2cb310b977583148c3972c2fa5b
IfSheKnows <ifejacobray@gmail.com>
2023-07-14 20:04:24 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *  main - Begin code
+ *  Return: 0 (Successful)
+ */
+int main(void)
+{
+	char n;
+
+	n = 'a';
+	while
+		(n <= 'z') {
+			putchar(n);
+			n++;
+		}
+	putchar('\n');
+	return (0);
+}
